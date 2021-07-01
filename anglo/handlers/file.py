60ef1c596566dea4f73f68359faecf57373808d6
Zenqi <zenaiwellplayed@gmail.com>
2021-07-01 23:17:32 +0800

    Add comments

--- anglo/handlers/file.py
+++ anglo/handlers/file.py
@@ -19,4 +19,4 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# This modue handles serving the application with different server adapters
+# This module is used for handling files, watching changes and more.
