aef6df9b1de479e127464f4d0484dea4026655c4
Zenqi <zenaiwellplayed@gmail.com>
2021-07-01 13:32:18 +0800

    Change copyright notive

--- anglo/request.py
+++ anglo/request.py
@@ -1,4 +1,4 @@
-#               Copyright (c) 2021 Anglo Developers.
+#               Copyright (c) 2021 Serum Studio.
 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
